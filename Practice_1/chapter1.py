a03544833c5d0c41a9b1dee0f449b212ea994197
irfan-official <irfanofficial.work@gmail.com>
2025-02-17 18:11:48 +0600

    chapter1 complete

---
+++ Practice_1/chapter1.py
@@ -0,0 +1,26 @@
+# Data types 
+# 1. 
+
+# String 
+a = '''Tripple single quotes'''
+b = """Tripple double quotes"""
+c = 'single quotes'
+d = "double quotes"
+
+print(f"a = {a}")
+print(f"b = {b}")
+print(f"c = {c}")
+print(f"d = {d}")
+
+
+import library as library
+library.one()
+
+import library as cp2
+cp2.two()
+
+from library import one as first, two as second;
+first()
+second()
+
+
---
+++ Practice_1/library.py
@@ -0,0 +1,10 @@
+
+
+
+
+def one():
+  print(f"one function is called")
+
+def two():
+  print(f"two function is called")
+  
